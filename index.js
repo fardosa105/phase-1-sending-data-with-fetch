284d6c9893a34bd7d57d75188cd92f72cf5431f7
+ <fadosa.abdirhman@student.moringaschool>
2024-07-09 07:00:32 +0300

    modify

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your code here
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script src="path/to/your/script.js"></script>
+</body>
+</html>
